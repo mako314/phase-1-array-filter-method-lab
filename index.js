2e78c2e38b3037d10aa25b4ecb439a16ab687eb1
mako314 <119079347+mako314@users.noreply.github.com>
2023-05-03 18:07:14 -0400

    finish find fuzzy and match functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const driv = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function findMatching(drivers,myString){
+  return drivers.filter(function(driver){
+  return driver.toUpperCase() === myString.toUpperCase();
+  })
+  }
+
+  function fuzzyMatch(drivers, myString){
+    return drivers.filter(function(driver){
+    return driver.charAt([0,1]) === myString.charAt([0,1]);
+    })
+  }
+
+  function matchName(drivers, myString){
+    return drivers.filter(function(driver){
+    return driver.name === myString;
+    
+    })
+  
+
+  }
+  
